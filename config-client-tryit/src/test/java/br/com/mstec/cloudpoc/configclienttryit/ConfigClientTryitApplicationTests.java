16b6de0b57cb3aafcb2b7f300764314a995def0a
Marcos Gabriel <mvm.gabriel@gmail.com>
2022-07-12 21:53:51 -0300

    Server Config, Config Repo e Client

---
+++ config-client-tryit/src/test/java/br/com/mstec/cloudpoc/configclienttryit/ConfigClientTryitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mstec.cloudpoc.configclienttryit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientTryitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
